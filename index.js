f93a6278c9e1b23725fdd04b5af0080b5f6dba14
banglapress <zafar21@gmail.com>
2022-06-29 06:28:29 +0600

    orders get method added

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ async function run() {
         })
 
 
-        app.get('/orders', async (req, res) => {
-            const cursor = ordersCollection.find({});
-            const orders = await cursor.toArray();
-            res.json(orders);
-        })
+        // app.get('/orders', async (req, res) => {
+        //     const cursor = ordersCollection.find({});
+        //     const orders = await cursor.toArray();
+        //     res.json(orders);
+        // })
 
         //--ok
 
